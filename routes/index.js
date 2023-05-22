5f05f1c1ce963efe9f218808f621c7082985e72b
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-05-22 17:03:33 +0700

    CRUD using Jquery Done

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/', (req,res)=>{
   res.render('indexVanilla')
 })
 
+router.get('/jquery',(req,res)=>{
+  res.render('indexJquery')
+})
+
 
 // db.data.insertMany([{id:1, string:'Testing data', integer:13, float:1.21,date: new Date("2023-01-30"), boolean:true},{id:2, string:'Testing', integer:12, float:2.21,date: new Date("2017-10-12"),boolean:true},{id:3, string:'data', integer:20, float:4.1,date: new Date("2022-12-30"),boolean:false},{id:4, string:'ini', integer:14, float:1.5,date: new Date("2023-01-08"), boolean:true}])
 
